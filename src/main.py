01f2728e85f59b44f0439dadb10716a9d83b57af
nitahieb <nitahieb@gmail.com>
2025-09-16 17:24:28 -0700

    handle nonunicode characters

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from cli import ParseArgs
 from services import getEmails, getSummary
+import sys
 
 
 
 def main():
+    sys.stdout.reconfigure(encoding='utf-8')
     args = ParseArgs()
     if args.mode == "overview":
         summary = getSummary(args.searchterm, args.sortby, args.email, args.searchnumber)
